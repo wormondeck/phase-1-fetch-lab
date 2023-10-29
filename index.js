9abf0b7e08c2d765737ed776a3138076796eb81a
Ronney Arias <ronneyarias@gmail.com>
2023-10-29 11:47:59 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+   return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(data => renderBooks(data));
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
